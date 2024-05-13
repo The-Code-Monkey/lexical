b0290f2c19923d0389a14570e8bade6fa0a5acf1
Andy Wilson <andy@builtbypixel.com>
2024-05-13 09:53:17 +0100

    fix: linting

--- docs/app/page.tsx
+++ docs/app/page.tsx
@@ -1,20 +1,25 @@
-'use client'
+"use client";
+
+import "@techstack/lexical/css";
+
+import EditorContainer from "@techstack/lexical";
+import { useState } from "react";
 
 import styles from "./page.module.css";
 
-import EditorContainer from "@techstack/lexical";
-import {useState} from "react";
-
-import "@techstack/lexical/css"
-
 export default function Home() {
-  const [state, setState] = useState("testing")
+  const [state, setState] = useState("testing");
 
   return (
     <main className={styles.main}>
-        {typeof window !== "undefined" && (
-            <EditorContainer name="test" onChange={setState} value={state} />
-        )}
+      {typeof window !== "undefined" && (
+        <EditorContainer
+          name="test"
+          onChange={setState}
+          placeholder="Test"
+          value={state}
+        />
+      )}
     </main>
   );
 }
